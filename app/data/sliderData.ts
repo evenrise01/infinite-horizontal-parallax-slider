be9df40fdd6b69b3167f34e2269ae0200ceb9b2d
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-11-05 12:00:45 +0530

    added assets and constant data

---
+++ app/data/sliderData.ts
@@ -0,0 +1,42 @@
+export const sliderData = [
+    {
+        title: "Echoes of Silence",
+        img: "/img-01.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Floral Circuit",
+        img: "/img-02.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Synthetic Horizon",
+        img: "/img-03.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Portal Sequence",
+        img: "/img-04.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Projected Memory",
+        img: "/img-05.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Fractured Self",
+        img: "/img-06.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Astral Convergence",
+        img: "/img-07.jpg",
+        url: "/sample-project"
+    },
+    {
+        title: "Fading Room",
+        img: "/img-08.jpg",
+        url: "/sample-project"
+    },
+]
